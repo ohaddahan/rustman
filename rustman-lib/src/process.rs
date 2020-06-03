0b20caca4cdb023f17a77dafb491e0e24fd3b3fb
Ohad Dahan <admin@example.com>
2020-06-03 21:12:02 +0300

    Added Process tests

--- rustman-lib/src/process.rs
+++ rustman-lib/src/process.rs
@@ -1,6 +1,7 @@
 use std::io::prelude::*;
+use std::str;
 use std::error::Error;
-use std::process::{Command, Stdio};
+use std::process::{Command, Stdio, Child, Output};
 use std::env;
 use std::path::Path;
 use std::collections::HashMap;
@@ -43,7 +44,7 @@ impl Process {
         out_expanded_command
     }
 
-    pub fn run(&self, options: Option<HashMap<String, String>>) -> String {
+    pub fn run(&self, options: Option<HashMap<String, String>>) -> Child {
         let mut env = self.env.clone();
         match options {
             Some(i) => env.extend(i),
@@ -55,18 +56,15 @@ impl Process {
         let output = Command::new("sh").
             arg("-c").
             arg(cmd).
+            envs(&env).
             stdout(Stdio::piped()).
             spawn().
             expect("failed to execute process");
         self.chdir(path.into_os_string().into_string().unwrap());
-        let mut s = String::new();
-        match output.stdout.unwrap().read_to_string(&mut s) {
-            Err(e) => panic!("couldn't read wc stdout: {}", e),
-            Ok(_) => s
-        }
+        output
     }
 
-    pub fn exec(&mut self, options: Option<HashMap<String, String>>) {
+    pub fn exec(&mut self, options: Option<HashMap<String, String>>) -> String {
         let mut env = self.env.clone();
         match options {
             Some(i) => env.extend(i),
@@ -77,11 +75,12 @@ impl Process {
         }
         self.chdir(self.cwd());
         let cmd = self.expanded_command(Some(&self.env));
-        let _output = Command::new("sh").
+        let output = Command::new("sh").
             arg("-c").
             arg(cmd).
             output().
             expect("failed to execute process");
+        str::from_utf8(&output.stdout).unwrap().to_string()
     }
 
     fn chdir(&self, in_cwd: String) {
@@ -106,17 +105,21 @@ impl Process {
 #[cfg(test)]
 mod tests {
     use super::*;
+
     static TEST_BIN: &'static str = "tests/test.sh";
+    static ENV_BIN:  &'static str = "tests/env.sh";
+    static ECHO_BIN: &'static str = "tests/echo.sh";
+    static UTF8_BIN: &'static str = "tests/utf8.sh";
 
-    #[test]
-    fn test_runs_the_process() {
-        let process = Process::new(TEST_BIN.to_string(), None, None, None);
-        assert_eq!("testing\n", process.run(None));
-
+    fn run(process: Process, options: Option<HashMap<String, String>>) -> String {
+        let mut child = process.run(options);
+        let output = child.wait().unwrap();
+        let mut s = String::new();
+        match child.stdout.unwrap().read_to_string(&mut s) {
+            Err(e) => panic!("couldn't read wc stdout: {}", e),
+            Ok(_) => s
+        }
     }
-    //process = Foreman::Process.new(resource_path("bin/test"))
-    //expect(run(process)).to eq("testing\n")
-    //end
 
     #[test]
     fn test_cwd() {}
@@ -128,4 +131,77 @@ mod tests {
     fn test_run() {}
     #[test]
     fn test_expanded_command() {}
+
+    #[test]
+    fn test_runs_the_process() {
+        let process = Process::new(TEST_BIN.to_string(), None, None, None);
+        assert_eq!("testing\n", run(process, None));
+
+    }
+
+    #[test]
+    fn test_can_set_environment() {
+        let mut env: HashMap<String, String> = HashMap::new();
+        env.insert("FOO".to_string(), "bar".to_string());
+        let process = Process::new(format!("{} FOO", ENV_BIN), None, None, Some(env));
+        assert_eq!("bar\n", run(process, None));
+    }
+
+    #[test]
+    fn test_can_handle_env_vars_in_the_command() {
+        let hello = String::from("Olá");
+
+        let mut env: HashMap<String, String> = HashMap::new();
+        env.insert("FOO".to_string(), "bar".to_string());
+        let process = Process::new(format!("{} $FOO", ECHO_BIN), None, None, Some(env));
+        assert_eq!("bar\n", run(process, None));
+    }
+
+    #[test]
+    fn test_can_handle_per_run_env_vars_in_the_command() {
+        let mut env: HashMap<String, String> = HashMap::new();
+        env.insert("FOO".to_string(), "bar".to_string());
+        let process = Process::new(format!("{} $FOO", ECHO_BIN), None, None, None);
+        assert_eq!("bar\n", run(process, Some(env)));
+    }
+
+    //#[test]
+    //fn test_should_output_utf8_properly() {
+    //    let process = Process::new(UTF8_BIN.to_string(), None, None, None);
+    //    assert_eq!(str::from_utf8(b"\\xE2").unwrap(), run(process, None));
+    //}
+
+    #[test]
+    fn test_can_expand_env_in_the_command() {
+        let mut env: HashMap<String, String> = HashMap::new();
+        env.insert("FOO".to_string(), "bar".to_string());
+        let process = Process::new("command $FOO $BAR".to_string(), None, None, Some(env));
+        assert_eq!("command bar $BAR", process.expanded_command(None));
+    }
+
+    #[test]
+    fn test_can_expand_extra_env_in_the_command() {
+        let mut env: HashMap<String, String> = HashMap::new();
+        env.insert("FOO".to_string(), "bar".to_string());
+        let process = Process::new("command $FOO $BAR".to_string(), None, None, Some(env));
+        let mut env: HashMap<String, String> = HashMap::new();
+        env.insert("BAR".to_string(), "qux".to_string());
+        assert_eq!("command bar qux", process.expanded_command(Some(&env)));
+    }
+
+    #[test]
+    fn test_can_execute() {
+        let mut process = Process::new(TEST_BIN.to_string(), None, None, None);
+        assert_eq!("testing\n", process.exec(None));
+    }
+
+
+
+    #[test]
+    fn test_can_execute_with_env() {
+        let mut env: HashMap<String, String> = HashMap::new();
+        env.insert("FOO".to_string(), "bar".to_string());
+        let mut process = Process::new(format!("{} FOO", ENV_BIN), None, None, None);
+        assert_eq!("bar\n", process.exec(Some(env)));
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,33 @@
 extern crate rustman_lib;
+use std::str;
 use rustman_lib::procfile::Procfile;
+use rustman_lib::process::Process;
 use std::process::Command;
 use std::collections::HashMap;
 
-fn print(count: &mut i32, hash: &HashMap<String, String>, name: &str) {
-    *count += 1;
-    print!("{}: {} = {:#?}\n", name, count, hash);
-}
+//fn print(count: &mut i32, hash: &HashMap<String, String>, name: &str) {
+//    *count += 1;
+//    print!("{}: {} = {:#?}\n", name, count, hash);
+//}
 
 fn main() {
-    let mut count = 0;
-    let mut opt: HashMap<String, String> = HashMap::new();
-    let mut env: HashMap<String, String> = HashMap::new();
+    //let mut count = 0;
+    //let mut opt: HashMap<String, String> = HashMap::new();
+    //let mut env: HashMap<String, String> = HashMap::new();
 
     let cmd = "rustman-lib/tests/test.sh";
 
-    let output = Command::new("sh").
-        arg("-c").
-        arg(&cmd).
-        spawn().
-        expect("failed to execute process");
-    print!("output = {:#?}", output);
+    //let output = Command::new(cmd).
+    //    output().
+    //    expect("failed to execute process");
+    //print!("output = {:#?}", output);
+
+    //let output = Command::new("sh").
+    //    arg("-c").
+    //    arg(&cmd).
+    //    spawn().
+    //    expect("failed to execute process");
+    //print!("output = {:#?}", output);
 
     let output = Command::new("sh").
         arg("-c").
@@ -29,7 +36,7 @@ fn main() {
         expect("failed to execute process");
 
 
-    print!("output = {:#?}", output);
+    print!("output = {}", str::from_utf8(&output.stdout).unwrap());
     //let procfile = Procfile::new(String::from("rustman-lib/tests/Procfile1.test"));
     //println!("procfile:\n{}", procfile);
 }
